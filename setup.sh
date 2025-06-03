0f91a0c93eacd0262f5ba42e23a7f4d87bcc99cc
Abdul Rehman <L1S22BSSE0184@ucp.com>
2025-06-03 15:45:30 +0500

    my initial commit

---
+++ setup.sh
@@ -0,0 +1,8 @@
+git init
+git branch -m develop 
+mkdir ./src ./styles ./asserts ./bin  ./node_modules 
+touch ./.gitignore ./src/index.html ./styles/index.css
+git config --local user.name "Abdul Rehman"
+git config -- local user.email "l1s22bsse0184@ucp.edu.pk"
+git config --local core.editor "notepad"
+git remote add origin https://github.com/Khawaja-0184/s2-w11-class3.git
